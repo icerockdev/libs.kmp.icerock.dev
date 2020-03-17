d2e3458d2a68aa4d2551ccda63445a6005a5c7d8
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-03-17 10:51:38 +0700

    CNAME CI config

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.afcc3901302c38e0be9ff0a6fcf25786.js"
+  "/precache-manifest.c3508918d30c60b558aa57298ba7d3b9.js"
 );
 
 self.addEventListener('message', (event) => {
