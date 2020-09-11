69e22f14b8b269fbb8a7906c5254cace8d92907f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-09-11 19:13:36 +0700

    update data

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b10d7eb87bac8fd2dfb755e832c2e1c1.js"
+  "/precache-manifest.76c1ed9c9189d43b1ac70c8272db72cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
