2bb294951ad50daf1986354f68ba6a1a714b2fdd
GitHub Action <action@github.com>
2020-09-11 12:15:15 +0000

    Update versions

---
+++ docs/precache-manifest.b10d7eb87bac8fd2dfb755e832c2e1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82768f23199607f9e3667840e7046b23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9581e59339882fa33af7",
+    "url": "/static/js/2.77b4471d.chunk.js"
+  },
+  {
+    "revision": "af5193266d2ee1adde7337630912a5d2",
+    "url": "/static/js/2.77b4471d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcefbfc55f5ebb4b440b",
+    "url": "/static/js/main.d8bfb23f.chunk.js"
+  },
+  {
+    "revision": "6852053052d3fd556710",
+    "url": "/static/js/runtime-main.0bb9494d.js"
+  }
+]);
