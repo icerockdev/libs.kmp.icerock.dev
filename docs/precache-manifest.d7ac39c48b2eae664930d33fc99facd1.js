0eed543b15a1fa7e06199b7855ba4fea47879a69
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-10-10 13:01:35 +0700

    remove duplicate and rebuild

---
+++ docs/precache-manifest.d7ac39c48b2eae664930d33fc99facd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d995c47c9078a3d6d4242b22348cafe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ba384be832a9909ae9e",
+    "url": "/static/js/2.fd38f8fc.chunk.js"
+  },
+  {
+    "revision": "af5193266d2ee1adde7337630912a5d2",
+    "url": "/static/js/2.fd38f8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7419f0a154f4d69b4e",
+    "url": "/static/js/main.be3a8757.chunk.js"
+  },
+  {
+    "revision": "6852053052d3fd556710",
+    "url": "/static/js/runtime-main.0bb9494d.js"
+  }
+]);
